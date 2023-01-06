6760b1990c4bed97e722b33d2b42aa426578b120
Matt Williams <email.m@ttwill.uk>
2023-01-06 17:55:36 +0000

    0.4.0

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -1,6 +1,13 @@
+declare type ParamKey = `p_${number}`;
+declare type Input = {
+    strings: string[];
+    expressions: unknown[];
+};
 declare type Output = {
     text: string;
-    parameters: Record<string, any>;
+    parameters: Record<ParamKey, any>;
+    i: number;
 };
-export default function cypher(strings: TemplateStringsArray, ...expressions: unknown[]): Output;
+export default function cypher(strings: TemplateStringsArray, ...expressions: Input["expressions"]): Output;
+export declare function shiftParameters(output: Output, shift: number): Output;
 export {};
